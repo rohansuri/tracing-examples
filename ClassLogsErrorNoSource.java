9c9c4c1fd17e9e79327b8054bc31cb4ed854c6a7
Rohan Suri <rohan.a.suri@gmail.com>
2018-11-09 01:00:24 +0530

    sample source file to trace for what codepath is logging the error Call stack for sys_write doesn't show the codepath correctly. Still trying to figure out

---
+++ ClassLogsErrorNoSource.java
@@ -0,0 +1,46 @@
+
+/*
+When we're getting logs from a class whose source we don't have.
+We want to find what code path is taken to log this line.
+Taking thread dumps isn't feasible since we'd have to time our jstack issuance right when we're
+in the codepath for the error logging line.
+Log line alone too is insufficient because it only has the class name that is logging, not the code path.
+(not the method)
+
+sudo /usr/share/bcc/tools/trace 'SyS_write (arg1==1) "%s", arg2' -U -p `pgrep -f ClassLogsErrorNoSource`
+
+Following Sasha's first example in his JPoint talk.
+ */
+
+/*
+checkout options that could be passed to perf-map-agent by browsing source of:
+perf-map-agent.c
+
+try using the newer jvmti agent support perf itself has out of the box?
+https://github.com/torvalds/linux/tree/master/tools/perf/jvmti
+ */
+public class ClassLogsErrorNoSource {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Start tracing and then press return here.");
+        System.in.read();
+        System.out.println("Program started");
+
+        while(true){
+            foo();
+            Thread.sleep(4_000);
+        }
+
+    }
+
+    public static void foo(){
+        bar();
+    }
+
+    public static void bar(){
+        baz();
+    }
+
+    public static void baz(){
+        System.out.println("Can't do X...ERROR");
+    }
+}
